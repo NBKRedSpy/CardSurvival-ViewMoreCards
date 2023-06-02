da92f506ae12e25ae29725e1d4e78e8df2257821
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2023-06-02 12:16:00 -0400

    Updated for v1.05

--- src/GraphicsManager_CardLine_Patch.cs
+++ src/GraphicsManager_CardLine_Patch.cs
@@ -22,26 +22,26 @@ namespace ViewMoreCards
                 GameObject gameObject;
 
                 //The Location card row (top)
-                gameObject = GameObject.Find("MainCanvas/SlotsCanvas/Environment/Locations");
+                gameObject = GameObject.Find("MainCanvas/ShakeParent/SlotsCanvas/Environment/Locations");
                 gameObject.transform.localScale = new Vector3(.6f, .6f, 1f);
                 rect = gameObject.GetComponent<RectTransform>();
                 rect.sizeDelta = new Vector2(625, 0);
 
                 //The base's inventory (Middle)
-                gameObject = GameObject.Find("MainCanvas/SlotsCanvas/Base/BaseViewport");
+                gameObject = GameObject.Find("MainCanvas/ShakeParent/SlotsCanvas/Base/BaseViewport");
                 gameObject.transform.localScale = new Vector3(.6f, .6f, 1f);
                 rect = gameObject.GetComponent<RectTransform>();
                 rect.sizeDelta = new Vector2(700, 0);
 
                 //Character equipment
-                gameObject = GameObject.Find("MainCanvas/PopupWindowsCanvas/CharacterWindow/DarkBG/ShadowAndPopupWithTitle/Content" +
+                gameObject = GameObject.Find("MainCanvas/ShakeParent/PopupWindowsCanvas/CharacterWindow/DarkBG/ShadowAndPopupWithTitle/Content" +
                     "/Cards/CharacterSlotsViewport");
                 gameObject.transform.localScale = new Vector3(.6f, .6f, 1f);
                 rect = gameObject.GetComponent<RectTransform>();
                 rect.sizeDelta = new Vector2(500, 0);
 
                 //---Blueprints cards
-                gameObject = GameObject.Find("MainCanvas/PopupWindowsCanvas/BlueprintModelsWindow/ShadowAndPopupWithTitle/Content" +
+                gameObject = GameObject.Find("MainCanvas/ShakeParent/PopupWindowsCanvas/BlueprintModelsWindow/ShadowAndPopupWithTitle/Content" +
                     "/Blueprints/BlueprintsViewport");
 
                 gameObject.transform.localScale = new Vector3(.6f, .6f, 1f);
@@ -69,23 +69,23 @@ namespace ViewMoreCards
             //to adjust the area as a whole.
 
             //The cards in the inventory (the right of the container card)
-            gameObject = GameObject.Find("MainCanvas/PopupWindowsCanvas/InventoryInspectionWindow/ShadowAndPopupWithTitle/Content/" +
+            gameObject = GameObject.Find("MainCanvas/ShakeParent/PopupWindowsCanvas/InventoryInspectionWindow/ShadowAndPopupWithTitle/Content/" +
                 "InspectionGroup/InventoryGroup/Inventory");
             gameObject.transform.localScale = new Vector3(.6f, .6f, 1f);
 
             //The box around the cards
-            gameObject = GameObject.Find("MainCanvas/PopupWindowsCanvas/InventoryInspectionWindow/ShadowAndPopupWithTitle/Content/" +
+            gameObject = GameObject.Find("MainCanvas/ShakeParent/PopupWindowsCanvas/InventoryInspectionWindow/ShadowAndPopupWithTitle/Content/" +
                 "InspectionGroup/InventoryGroup/Inventory/Frame");
             rect = gameObject.GetComponent<RectTransform>();
             rect.sizeDelta = new Vector2(800, 0);
 
             //The Cards area
-            gameObject = GameObject.Find("MainCanvas/PopupWindowsCanvas/InventoryInspectionWindow/ShadowAndPopupWithTitle/Content/" +
+            gameObject = GameObject.Find("MainCanvas/ShakeParent/PopupWindowsCanvas/InventoryInspectionWindow/ShadowAndPopupWithTitle/Content/" +
                 "InspectionGroup/InventoryGroup/Inventory/InventoryViewport");
             rect = gameObject.GetComponent<RectTransform>();
             rect.sizeDelta = new Vector2(790, 0);
 
-            gameObject = GameObject.Find("MainCanvas/PopupWindowsCanvas/InventoryInspectionWindow/ShadowAndPopupWithTitle/Content/InspectionGroup/InventoryGroup/Inventory/ScrollbarHorizontal");
+            gameObject = GameObject.Find("MainCanvas/ShakeParent/PopupWindowsCanvas/InventoryInspectionWindow/ShadowAndPopupWithTitle/Content/InspectionGroup/InventoryGroup/Inventory/ScrollbarHorizontal");
             gameObject.transform.localPosition = new Vector3(-992, 0, 0);
             rect = gameObject.GetComponent<RectTransform>();
             rect.sizeDelta = new Vector2(790, 20);
